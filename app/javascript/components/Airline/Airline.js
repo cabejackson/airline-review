4019bcf748e0b11ae527aa941f54e6874e583a63
cabejackson <jacksoncaleb.calebjackson@gmail.com>
2021-02-16 09:32:42 -0500

    updated airline to include initial resonse with slug and url

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const Airline = () => {
+const Airline = (props) => {
+    const [airline, setAirline] = useState({})
+    const [review, setReview] = useState({})
+
+    useEffect(() => {
+        // api/v1/airlines/:slug
+        //airline/united-airlines
+        // console.log(props)
+
+        const slug = props.match.params.slug
+        //console.log("this is slug:", slug)
+
+        const url = `/api/v1/airlines/${slug}`
+        //console.log("this is url:", url)
+
+        axios.get(url)
+            .then(resp => console.log(resp))
+            .catch(resp => console.log(resp))
+
+
+    }, [])
     return <div>This is the Airlines#show view for our app.</div>
 }
 
